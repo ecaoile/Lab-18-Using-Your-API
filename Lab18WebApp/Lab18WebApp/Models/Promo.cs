152351ecb740b9c4fd326db64be2d84ded6be81f
ecaoile <unfie555@gmail.com>
2018-10-22 01:47:06 -0700

    stopping point - done

---
+++ Lab18WebApp/Lab18WebApp/Models/Promo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab18WebApp.Models
+{
+    public class Promo
+    {
+        public double OrderSubTotal { get; set; }
+        public double PromoAmt { get; set; }
+        public string PromoId { get; set; }
+        public string PromotionName { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public double MinimumOrderValue { get; set; }
+        public string PromotionType { get; set; }
+    }
+}
